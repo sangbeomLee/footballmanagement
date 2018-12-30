a5af8a5d9f421527a6ad4777ce3e53275553a593
sangbeomLee <wing951@naver.com>
2018-12-30 18:55:45 +0900

    test

--- src/UserGUI/UserMain.java
+++ src/UserGUI/UserMain.java
@@ -1,10 +1,13 @@
 package UserGUI;
 
-public class UserMAin {
+public class UserMain {
 
+	public UserMain
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//test
+		//test
+		//test
 	}
 
 }
