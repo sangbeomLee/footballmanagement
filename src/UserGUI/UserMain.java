470720fa735126ef9348ea216bbb77333be6fa56
sangbeomLee <wing951@naver.com>
2018-12-30 21:24:13 +0900

    팀/개인 버튼 GUI
    팀/개인 버튼 GUI 완료

--- src/UserGUI/UserMain.java
+++ src/UserGUI/UserMain.java
@@ -3,10 +3,8 @@ package UserGUI;
 public class UserMain {
 	public static void main(String[] args) {
 	
-		//user
-		int a=0;
-		int b=0;
-		System.out.println(a+b);
+		ChoiceGUI choicegui = new ChoiceGUI();
+		
 	}
 
 }
